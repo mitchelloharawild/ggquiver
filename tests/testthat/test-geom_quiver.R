fd91e7f099f84d20e605cce07c29560a9a255808
Unknown <mitch.ohara-wild@monash.edu>
2017-08-10 09:59:01 +1000

    Updated unit tests

--- tests/testthat/test-geom_quiver.R
+++ tests/testthat/test-geom_quiver.R
@@ -1,10 +1,16 @@
 library(dplyr)
 context("geom_quiver")
 
-test_that("str_length is number of characters", {
-  expand.grid(x=seq(0,pi,pi/12), y=seq(0,pi,pi/12)) %>%
+test_that("Simple trig quiver plot", {
+  plotdata <- expand.grid(x=seq(0,pi,pi/12), y=seq(0,pi,pi/12)) %>%
     mutate(u = cos(x),
-           v = sin(y)) %>%
+           v = sin(y))
+
+  plotdata %>%
     ggplot(aes(x=x,y=y,u=u,v=v)) +
     geom_quiver()
+
+  plotdata %>%
+    ggplot(aes(x=x,y=y,u=u,v=v)) +
+    geom_quiver(center=TRUE)
 })
